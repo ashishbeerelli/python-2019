3ef3c512a5b5edf0db3ffc5d468e4e92780c5a07
ashishbeerelli <52096981+ashishbeerelli@users.noreply.github.com>
2019-06-28 16:40:47 +0530

    Add files via upload

---
+++ day6 222.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+dict={"name":["anil","akhil","dinesh","harsha","ajay","kranth"],
+    "email.Id":["anil@gmail.com","akhil@gmail.com","dinesh@gmail.com","harsha@gmail.com","ajay@gmail.com","kranth@gmail.com"],
+                 "p.no":[686535498,25775418,71745,5419814,1472285,58255],
+                 "address":["gfhscj","nfgths","ftgggfj","wnjhr","dgjtydt","bgffhhfgc"]}
+b=pd.DataFrame(dict)
+print(b)
+
+
+# In[ ]:
+
+
+
+
